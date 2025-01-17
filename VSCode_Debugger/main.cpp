299c7ee975c9c9458192ebd6f8af113bd6a0dba6
Gina Vasey <vaseygin@msu.edu>
2025-01-17 13:05:37 -0500

    starting debugger tutorial

---
+++ VSCode_Debugger/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+int main() {
+    cout << "Is there some debugging to do?" << endl;
+
+    double x = 5.3;
+    int y = 5;
+
+    double x;
+
+    double z = x + y;
+
+    if (z == 10.3) {
+        cout << "You got " << z << " That's correct!" << endl;
+    }
+    else {
+        cout << "You got " << z << ", but the answer should be 10.3" << endl;
+    }
+
+    return 0;
+}
